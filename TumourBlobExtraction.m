ed1f00a3f3807d694a4a6d2d07b34256e1754d4c
Adam Worrall <aworrall333@gmail.com>
2019-11-03 01:27:36 +0000

    Finished base algorithm

--- TumourBlobExtraction.m
+++ TumourBlobExtraction.m
@@ -1,5 +1,22 @@
 function imageOutput = TumourBlobExtraction(imageInput)
 %TUMOURBLOBEXTRACTION Summary of this function goes here
 %   Detailed explanation goes here
-end
 
+% Greyscale of image
+greyImage = rgb2gray(imageInput);
+% Apply high pass filter
+highPassKernal = [-1 -1 -1;-1 8.6 -1;-1 -1 -1];
+highPassImage = imfilter(greyImage, highPassKernal);;
+% Apply median filter
+medianImage = medfilt2(highPassImage);
+% Threshold segmentation
+thresholdValue = 100;
+thresholdImage = medianImage > thresholdValue;
+thresholdImage = imfill(thresholdImage, 'holes');
+imshow(thresholdImage)
+% Morphological operations
+imageMorph = bwmorph(thresholdImage, 'skel');
+imageMorph = bwmorph(imageMorph, 'branchpoints', 4);
+imageMorph = bwmorph(imageMorph, 'spur', 4);
+imageOutput = bwmorph(imageMorph, 'clean', inf);
+end
--- main.m
+++ main.m
@@ -1,5 +1,9 @@
 clc; clear;
 [filename pathname] = uigetfile({'*.jpg', '*.png'}, 'FileSelector');
+if filename == 0
+    return
+end
 fullpathname = strcat(pathname, filename);
 MRI_image = imread(fullpathname);
 Tumour_image = TumourBlobExtraction(MRI_image);
+imshow(Tumour_image);
